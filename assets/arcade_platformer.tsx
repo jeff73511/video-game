24cae9579540f4ab9d73640629adc9a9c13a614e
Jeff <jeff73511@msn.com>
2022-07-04 15:15:42 +0200

    Initial commit

---
+++ assets/arcade_platformer.tsx
@@ -0,0 +1,133 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="arcade_platformer" tilewidth="128" tileheight="128" tilecount="43" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="images/ground/Grass/grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="images/ground/Grass/grassCenter_round.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="images/ground/Grass/grassCenter.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="images/ground/Grass/grassCliff_left.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="images/ground/Grass/grassCliff_right.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="images/ground/Grass/grassCliffAlt_left.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="images/ground/Grass/grassCliffAlt_right.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="images/ground/Grass/grassCorner_left.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="images/ground/Grass/grassCorner_right.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="images/ground/Grass/grassHalf_left.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="images/ground/Grass/grassHalf_mid.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="images/ground/Grass/grassHalf_right.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="images/ground/Grass/grassHalf.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="images/ground/Grass/grassHill_left.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="images/ground/Grass/grassHill_right.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="images/ground/Grass/grassLeft.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="images/ground/Grass/grassMid.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="images/ground/Grass/grassRight.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="images/HUD/hudHeart_empty.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="images/HUD/hudHeart_full.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="images/HUD/hudHeart_half.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="images/HUD/hudX.png"/>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="images/items/coinBronze.png"/>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="images/items/coinGold.png"/>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="images/items/coinSilver.png"/>
+ </tile>
+ <tile id="30">
+  <image width="128" height="128" source="images/items/flagGreen_down.png"/>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="images/items/flagGreen1.png"/>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="images/items/flagGreen2.png"/>
+ </tile>
+ <tile id="41">
+  <image width="128" height="128" source="images/tiles/doorOpen_mid.png"/>
+ </tile>
+ <tile id="42">
+  <image width="128" height="128" source="images/tiles/doorOpen_top.png"/>
+ </tile>
+ <tile id="43">
+  <image width="128" height="128" source="images/tiles/grass.png"/>
+ </tile>
+ <tile id="44">
+  <image width="128" height="128" source="images/tiles/grass2.png"/>
+ </tile>
+ <tile id="45">
+  <image width="128" height="128" source="images/tiles/ladderMid.png"/>
+ </tile>
+ <tile id="46">
+  <image width="128" height="128" source="images/tiles/ladderTop.png"/>
+ </tile>
+ <tile id="47">
+  <image width="128" height="128" source="images/tiles/signExit.png"/>
+ </tile>
+ <tile id="48">
+  <image width="128" height="128" source="images/tiles/signLeft.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="128" source="images/tiles/signRight.png"/>
+ </tile>
+ <tile id="50">
+  <image width="128" height="128" source="images/tiles/torch1.png"/>
+ </tile>
+ <tile id="51">
+  <image width="128" height="128" source="images/tiles/torch2.png"/>
+ </tile>
+ <tile id="52">
+  <image width="128" height="128" source="images/tiles/water.png"/>
+ </tile>
+ <tile id="53">
+  <image width="128" height="128" source="images/tiles/waterTop_high.png"/>
+ </tile>
+ <tile id="54">
+  <image width="128" height="128" source="images/tiles/waterTop_low.png"/>
+ </tile>
+ <tile id="55">
+  <image width="128" height="128" source="images/tiles/boxCrate_double.png"/>
+ </tile>
+</tileset>
